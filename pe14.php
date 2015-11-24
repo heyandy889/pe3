5bb322e19b2c3e13ec8f43fbf6137a791108472c
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-24 17:10:20 -0500

    begin #14, longest collatz chain under a million

---
+++ pe14.php
@@ -0,0 +1,52 @@
+
+<style>
+body{
+	font-family:sans-serif;
+}
+</style>
+
+
+<?php
+
+ini_set("display_errors", TRUE);
+
+
+
+function nextCollatz($current){
+
+	$next = -1; 
+
+	if($current % 2 == 0){
+		$next = $current / 2;
+	}
+	else{
+		$next = 3 * $current + 1;
+	}
+
+	return $next;
+}
+
+
+echo("<a href='https://projecteuler.net/problem=14'>Project Euler - Problem 14</a>");
+echo("<br>");
+echo("<br>");
+
+$seed = 13;
+echo("seed: " . $seed);
+echo("<br>");
+$next = nextCollatz($seed);
+while($next != 1){
+	echo($next);
+	echo("<br>");
+	$next = nextCollatz($next);
+}
+
+
+
+
+
+
+
+
+?>
+
