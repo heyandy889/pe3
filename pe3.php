95edb0d187bf2689777c9375d4bf32af7d20fb2d
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-23 19:57:57 -0500

    calculate primes with php

---
+++ pe3.php
@@ -0,0 +1,59 @@
+Largest prime factor
+<br>
+Problem 3
+<br>
+The prime factors of 13195 are 5, 7, 13 and 29.
+<br>
+What is the largest prime factor of the number 600851475143 ?
+<br>
+<br>
+<br>
+<br>
+
+<?php
+$error_reporting = E_ALL | E_STRICT;
+ini_set('display_errors', True);
+
+function isPrime($target){
+	for($i=2; $i < sqrt($target); $i += 1){
+		$remainder = $target % $i;
+		if($remainder == 0){
+			return False;
+		}
+	}
+	return True;
+}
+
+
+$primes = array();
+
+echo("Is it prime?");
+echo("<br>");
+//for($i=2; $i<sqrt(600851475143); $i++){
+for($i=2; $i<sqrt(6008); $i++){
+
+	if(isPrime($i)){
+		$primes[] = $i;
+	}
+}
+
+
+print_r($primes);
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
